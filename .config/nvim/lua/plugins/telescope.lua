0d75adec2df493e0ba798b14d8956d314811d78b
Joseph Baca <jojobaca123@gmail.com>
2025-06-14 13:28:03 -0600

    adds Ctrl jk to telescope

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -1,6 +1,23 @@
 require('telescope').setup{}
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<leader>ff', builtin.find_files, {})
-vim.keymap.set('n', '<leader>fg', builtin.live_grep, {})
 vim.keymap.set('n', '<leader>fk', builtin.keymaps, {})
+
+
+
+require('telescope').setup{
+  defaults = {
+    mappings = {
+      i = { -- insert mode
+        ["<C-j>"] = require('telescope.actions').move_selection_next,
+        ["<C-k>"] = require('telescope.actions').move_selection_previous,
+      },
+      n = { -- normal mode
+        ["j"] = require('telescope.actions').move_selection_next,
+        ["k"] = require('telescope.actions').move_selection_previous,
+      },
+    },
+  },
+}
+
 
