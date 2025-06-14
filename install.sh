2fc31faca3dc3d1d3e83b9856ac079b19a9acabc
Joseph Baca <jojobaca123@gmail.com>
2025-06-14 13:39:00 -0600

    comments out alacritty

--- install.sh
+++ install.sh
@@ -6,10 +6,10 @@ DOTFILES_DIR="$(cd "$(dirname "$0")" && pwd)"
 echo "Linking .tmux.conf..."
 ln -sf "$DOTFILES_DIR/.tmux.conf" "$HOME/.tmux.conf"
 
-echo "Linking Alacritty config..."
-mkdir -p "$HOME/.config/alacritty"
-ln -sf "$DOTFILES_DIR/.config/alacritty/alacritty.toml" "$HOME/.config/alacritty/alacritty.toml"
-
+# echo "Linking Alacritty config..."
+# mkdir -p "$HOME/.config/alacritty"
+# ln -sf "$DOTFILES_DIR/.config/alacritty/alacritty.toml" "$HOME/.config/alacritty/alacritty.toml"
+#
 echo "Linking Neovim config..."
 mkdir -p "$HOME/.config/nvim"
 ln -sf "$DOTFILES_DIR/.config/nvim/init.lua" "$HOME/.config/nvim/init.lua"
