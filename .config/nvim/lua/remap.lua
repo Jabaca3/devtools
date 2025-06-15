61ff93cc1644fb9e548888b521d7e22b73b60703
Joseph Baca <jojobaca123@gmail.com>
2025-06-14 20:35:56 -0600

    adds fun remaps

--- .config/nvim/lua/remap.lua
+++ .config/nvim/lua/remap.lua
@@ -1,3 +1,20 @@
 vim.g.mapleader = " "
 vim.keymap.set("n", "<leader>ds", vim.cmd.Ex)  -- file explorer
 vim.keymap.set("i", "jk", "<Esc>", { noremap = true})
+
+-- Highlights move together
+vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
+
+-- Page jumps keeps cursor in the middle
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+
+-- Search terms to stay in the middle
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
+-- Copy for system clipboard
+vim.keymap.set("n", "<leader>y", "\"+y")
+vim.keymap.set("v", "<leader>y", "\"+y")
+vim.keymap.set("n", "<leader>y", "\"+Y")
